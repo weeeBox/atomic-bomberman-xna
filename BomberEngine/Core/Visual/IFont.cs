ff1e62a5129b91c638b5e4265f27afb680da400a
Alex Lementuev <a.lementuev@gmail.com>
2013-05-14 21:03:00 -0700

    Progress

---
+++ BomberEngine/Core/Visual/IFont.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BomberEngine.Core.Visual
+{
+    public interface IFont
+    {
+        string[] WrapString(String text, int wrapWidth);
+        int StringWidth(String str);
+        int FontHeight();
+        int LineOffset();
+
+        void DrawString(String text, float x, float y);
+        void DrawString(String text, float x, float y, TextAlign textAlign);
+    }
+}
---
+++ BomberEngine/Core/Visual/Text.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BomberEngine.Debugging;
+using BomberEngine.Util;
+
+namespace BomberEngine.Core.Visual
+{
+    public enum TextAlign
+    {
+        LEFT = 1 << 0,
+        HCENTER = 1 << 1,
+        RIGHT = 1 << 2,
+        TOP = 1 << 3,
+        VCENTER = 1 << 4,
+        BOTTOM = 1 << 5
+    }
+
+    public class Text : VisualElement
+    {
+        protected struct FormattedString
+        {
+            public String text;
+            public float width;
+
+            public FormattedString(String text, float width)
+            {
+                this.text = text;
+                this.width = width;
+            }
+        }
+
+        public String text;
+        public IFont font;
+
+        protected FormattedString[] formattedStrings;
+
+        public Text(IFont font)
+        {
+            Debug.Assert(font != null);
+
+            this.font = font;
+            width = Constants.UNDEFINED;
+            height = Constants.UNDEFINED;
+        }
+
+        public void SetString(String newString)
+        {
+            SetString(newString, Int32.MaxValue);
+        }
+
+        public virtual void SetString(String newString, int wrapWidth)
+        {
+            text = newString;
+
+            String[] strings = font.WrapString(text, wrapWidth);
+            int stringsCount = strings.Length;
+            formattedStrings = new FormattedString[stringsCount];
+            for (int i = 0; i < stringsCount; ++i)
+            {
+                String str = strings[i];
+                int strWidth = font.StringWidth(str);
+                if (strWidth > width)
+                    width = strWidth;
+                formattedStrings[i] = new FormattedString(str, strWidth);
+            }
+            height = (font.FontHeight() + font.LineOffset()) * formattedStrings.Length - font.LineOffset();
+        }
+
+        public String getString()
+        {
+            return text;
+        }
+
+        public void setAlign(TextAlign textAlign)
+        {   
+            if ((textAlign & TextAlign.RIGHT) != 0)
+            {
+                alignX = ALIGN_MAX;
+            }
+            else if ((textAlign & TextAlign.HCENTER) != 0)
+            {
+                alignX = ALIGN_CENTER;
+            }
+            else
+            {
+                alignX = ALIGN_MIN;
+            }
+
+            if ((textAlign & TextAlign.BOTTOM) != 0)
+            {
+                alignY = ALIGN_MAX;
+            }
+            else if ((textAlign & TextAlign.VCENTER) != 0)
+            {
+                alignY = ALIGN_CENTER;
+            }
+            else
+            {
+                alignY = ALIGN_MIN;
+            }
+        }
+
+        public override void Draw(Context context)
+        {
+            PreDraw(context);
+
+            float dx;
+            float dy = 0;
+            int itemHeight = font.FontHeight();
+
+            for (int i = 0; i < formattedStrings.Length; i++)
+            {
+                FormattedString str = formattedStrings[i];
+                dx = alignX * (width - str.width);
+                font.DrawString(str.text, dx, dy);
+                dy += itemHeight + font.LineOffset();
+            }
+
+            PostDraw(context);
+        }
+    }
+}
