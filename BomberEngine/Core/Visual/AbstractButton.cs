4c5d5b5172b04ee0d3cec4b930681183b08a0b00
Alex Lementuev <a.lementuev@gmail.com>
2013-05-15 21:22:04 -0700

    Progress

---
+++ BomberEngine/Core/Visual/AbstractButton.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using BomberEngine.Core.Input;
+
+namespace BomberEngine.Core.Visual
+{
+    public interface IButtonDelegate
+    {
+        void OnButtonPress(AbstractButton button);
+    }
+
+    public class AbstractButton : View
+    {
+        protected IButtonDelegate buttonDelegate;
+
+        public AbstractButton(IButtonDelegate buttonDelegate)
+        {
+            this.buttonDelegate = buttonDelegate;
+        }
+
+        public override bool OnKeyPressed(Keys key)
+        {
+            if (key == Keys.Enter)
+            {
+                OnPress();
+                return true;
+            }
+
+            return base.OnKeyPressed(key);
+        }
+
+        public override bool OnKeyReleased(Keys key)
+        {
+            if (key == Keys.Enter)
+            {
+                OnRelease();
+                return true;
+            }
+
+            return base.OnKeyReleased(key);
+        }
+
+        public override bool OnButtonPressed(ButtonEvent e)
+        {   
+            if (e.button == Buttons.A)
+            {
+                OnPress();
+                return true;
+            }
+            
+            return base.OnButtonPressed(e);
+        }
+
+        public override bool OnButtonReleased(ButtonEvent e)
+        {
+            if (e.button == Buttons.A)
+            {
+                OnRelease();
+                return true;
+            }
+
+            return base.OnButtonReleased(e);
+        }
+
+        protected virtual void OnPress()
+        {
+            if (buttonDelegate != null)
+            {
+                buttonDelegate.OnButtonPress(this);
+            }
+        }
+
+        protected virtual void OnRelease()
+        {
+        }
+    }
+}
