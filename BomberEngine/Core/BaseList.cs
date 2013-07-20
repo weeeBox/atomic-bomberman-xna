a5c255dac1b8991bd9a5c3379199ee31cffd3a21
Alex Lementuev <a.lementuev@gmail.com>
2013-07-20 14:19:06 -0700

    Added BaseList

---
+++ BomberEngine/Core/BaseList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BomberEngine.Util;
+
+namespace BomberEngine.Core
+{
+    public abstract class BaseList<T> : Collection<T> where T : class
+    {
+        protected List<T> list;
+
+        protected T nullElement;
+        protected int removedCount;
+
+        protected BaseList()
+        {
+        }
+
+        protected BaseList(T nullElement)
+            : this(nullElement, 0)
+        {
+        }
+
+        protected BaseList(T nullElement, int capacity)
+        {
+            this.nullElement = nullElement;
+            list = new List<T>(capacity);
+        }
+
+        public virtual bool Add(T updatable)
+        {
+            list.Add(updatable);
+            return true;
+        }
+
+        public virtual bool Remove(T updatable)
+        {
+            int index = list.IndexOf(updatable);
+            if (index != -1)
+            {
+                Remove(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual T Get(int index)
+        {
+            return list[index];
+        }
+
+        public virtual int IndexOf(T updatable)
+        {
+            return list.IndexOf(updatable);
+        }
+
+        public virtual void Remove(int index)
+        {
+            ++removedCount;
+            list[index] = nullElement;
+        }
+
+        public virtual void Clear()
+        {
+            list.Clear();
+        }
+
+        public virtual int Count()
+        {
+            return list.Count - removedCount;
+        }
+
+        public virtual bool Contains(T updatable)
+        {
+            return list.Contains(updatable);
+        }
+
+        public virtual bool IsNull()
+        {
+            return false;
+        }
+
+        protected void ClearRemoved()
+        {
+            for (int i = 0; i < list.Count; ++i)
+            {
+                if (list[i] == nullElement)
+                {
+                    list.RemoveAt(i);
+                    --removedCount;
+
+                    if (removedCount == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- BomberEngine/Core/BaseUpdatableList.cs
+++ BomberEngine/Core/BaseUpdatableList.cs
@@ -6,26 +6,20 @@ using BomberEngine.Util;
 
 namespace BomberEngine.Core
 {
-    public abstract class BaseUpdatableList<T> : IUpdatable, Collection<T> where T : class, IUpdatable
+    public abstract class BaseUpdatableList<T> : BaseList<T>, IUpdatable where T : class, IUpdatable
     {   
-        protected List<T> list;
-
-        private T nullElement;
-        private int removedCount;
-
         protected BaseUpdatableList()
         {
         }
 
         protected BaseUpdatableList(T nullElement)
-            : this(nullElement, 0)
+            : base(nullElement, 0)
         {
         }
 
         protected BaseUpdatableList(T nullElement, int capacity)
+            : base(nullElement, capacity)
         {   
-            this.nullElement = nullElement;
-            list = new List<T>(capacity);
         }
 
         public virtual void Update(float delta)
@@ -39,77 +33,6 @@ namespace BomberEngine.Core
             if (removedCount > 0)
             {
                 ClearRemoved();
-            }
-        }
-
-        public virtual bool Add(T updatable)
-        {
-            list.Add(updatable);
-            return true;
-        }
-
-        public virtual bool Remove(T updatable)
-        {
-            int index = list.IndexOf(updatable);
-            if (index != -1)
-            {
-                Remove(index);
-                return true;
-            }
-
-            return false;
-        }
-
-        public virtual T Get(int index)
-        {
-            return list[index];
-        }
-
-        public virtual int IndexOf(T updatable)
-        {
-            return list.IndexOf(updatable);
-        }
-
-        public virtual void Remove(int index)
-        {
-            ++removedCount;
-            list[index] = nullElement;
-        }
-
-        public virtual void Clear()
-        {
-            list.Clear();
-        }
-
-        public virtual int Count()
-        {
-            return list.Count - removedCount;
-        }
-
-        public virtual bool Contains(T updatable)
-        {
-            return list.Contains(updatable);
-        }
-
-        public virtual bool IsNull()
-        {
-            return false;
-        }
-
-        private void ClearRemoved()
-        {
-            for (int i = 0; i < list.Count; ++i)
-            {
-                if (list[i] == nullElement)
-                {
-                    list.RemoveAt(i);
-                    --removedCount;
-
-                    if (removedCount == 0)
-                    {
-                        break;
-                    }
-                }
             }
         }
     }
