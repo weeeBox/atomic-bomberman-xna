048f0989048dce9d8be00b7bd9bdefd7af21119f
Alex Lementuev <a.lementuev@gmail.com>
2013-07-21 19:40:31 -0700

    Round result screen progress

---
+++ Bomberman/Bomberman/Game/Screens/RoundResultScreen.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BomberEngine.Game;
+using BomberEngine.Core.Visual;
+using BomberEngine.Core.Assets.Types;
+
+namespace Bomberman.Game.Screens
+{
+    public class RoundResultScreen : Screen
+    {
+        public enum ButtonId
+        {
+            Exit,
+            Continue
+        }
+
+        public RoundResultScreen(ButtonDelegate buttonDelegate)
+        {
+            View contentView = new View(64, 48, 512, 384);
+
+            // table
+            View tableView = new View(0, 25, contentView.width, 330);
+
+            float nameColWidth = 348;
+            float winsColWidth = 0.5f * (tableView.width - nameColWidth);
+            float suicidesColWidth = winsColWidth;
+
+            Font font = Helper.fontButton;
+
+            TextView textView = new TextView(font, "PLAYER");
+            textView.alignX = View.ALIGN_CENTER;
+            textView.x = 0.5f * nameColWidth;
+            tableView.AddView(textView);
+
+            textView = new TextView(font, "WINS");
+            textView.alignX = View.ALIGN_CENTER;
+            textView.x = nameColWidth + 0.5f * winsColWidth;
+            tableView.AddView(textView);
+
+            textView = new TextView(font, "SUICIDES");
+            textView.alignX = View.ALIGN_CENTER;
+            textView.x = nameColWidth + winsColWidth + 0.5f * suicidesColWidth;
+            tableView.AddView(textView);
+
+            contentView.AddView(tableView);
+
+            // buttons
+            View buttons = new View(0.5f * contentView.width, contentView.y + contentView.height, 0, 0);
+            buttons.alignX = View.ALIGN_CENTER;
+            buttons.alignY = View.ALIGN_MAX;
+
+            Button button = new TextButton("EXIT", font, 0, 0, 100, 20);
+            button.id = (int)ButtonId.Exit;
+            button.buttonDelegate = buttonDelegate;
+            SetCancelButton(button);
+            buttons.AddView(button);
+
+            button = new TextButton("START!", font, 0, 0, 100, 20);
+            button.id = (int)ButtonId.Continue; ;
+            button.buttonDelegate = buttonDelegate;
+            SetConfirmButton(button);
+            buttons.AddView(button);
+
+            buttons.LayoutHor(20);
+            buttons.ResizeToFitViews();
+            contentView.AddView(buttons);
+
+            AddView(contentView);
+        }
+    }
+}
