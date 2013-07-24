2f2f90a714dd8efcf2be3094f6e6a1b431cb944d
Alex Lementuev <a.lementuev@gmail.com>
2013-07-23 20:23:33 -0700

    Animation instance progress

--- Bomberman/Bomberman/Content/AnimationInstance.cs
+++ Bomberman/Bomberman/Content/AnimationInstance.cs
@@ -9,26 +9,38 @@ namespace Bomberman.Content
 {
     public class AnimationInstance : IUpdatable, IResettable
     {
-        private Animation mAnimation;
-        private AnimationGroup mGroup;
-
-        private float mFrameTime;
-        private int mFrameIndex;
-        private float mSpeedMultiplier;
-
-        public AnimationInstance(Animation animation)
+        public enum Mode
         {
-            mAnimation = animation;
+            Normal,
+            Looped,
+        }
+
+        private Animation m_Animation;
+        private AnimationGroup m_Group;
+
+        private float m_FrameTime;
+        private int m_FrameIndex;
+        private float m_SpeedMultiplier;
+
+        private Mode m_Mode;
+
+        public void Init(Animation animation)
+        {
+            m_Animation = animation;
             Reset();
         }
 
         public void Update(float delta)
         {
-            mFrameTime += delta * mSpeedMultiplier;
-            if (mFrameTime >= mGroup.frames[mFrameIndex].duration) // TODO: handle skipped frames
+            m_FrameTime += delta * m_SpeedMultiplier;
+            if (m_FrameTime >= m_Group.frames[m_FrameIndex].duration) // TODO: handle skipped frames
             {
-                mFrameTime = 0.0f;
-                mFrameIndex = (mFrameIndex + 1) % mGroup.frames.Length;
+                m_FrameTime = 0.0f;
+                if (m_FrameIndex == m_Group.frames.Length - 1)
+                {
+                    if (m_Mode == Mode.Looped)
+                        m_FrameIndex = 0;
+                }
             }
         }
 
@@ -36,32 +48,39 @@ namespace Bomberman.Content
         {
             Reset();
 
-            AnimationGroup group = mAnimation.Group(name);
+            AnimationGroup group = m_Animation.Group(name);
             Debug.Assert(group != null, "Can't find group: " + name);
         }
 
         public void Reset()
         {
-            mFrameIndex = 0;
-            mFrameTime = 0.0f;
-            mGroup = null;
-            mSpeedMultiplier = 1.0f;
+            m_FrameIndex = 0;
+            m_FrameTime = 0.0f;
+            m_Group = null;
+            m_SpeedMultiplier = 1.0f;
+            m_Mode = Mode.Normal;
         }
 
         public AnimationGroup Group
         {
-            get { return mGroup; }
+            get { return m_Group; }
         }
 
         public float FrameIndex
         {
-            get { return mFrameIndex; }
+            get { return m_FrameIndex; }
         }
 
         public float SpeedMultiplier
         {
-            get { return mSpeedMultiplier; }
-            set { mSpeedMultiplier = value; }
+            get { return m_SpeedMultiplier; }
+            set { m_SpeedMultiplier = value; }
+        }
+
+        public Mode mode
+        {
+            get { return m_Mode; }
+            set { m_Mode = value; }
         }
     }
 }
