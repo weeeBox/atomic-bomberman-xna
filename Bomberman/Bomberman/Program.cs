9084b21add4b14ff0b91db8611e0169024dbd987
Alex Lementuev <a.lementuev@gmail.com>
2013-08-04 21:37:08 -0700

    Added debug hack

--- Bomberman/Bomberman/Program.cs
+++ Bomberman/Bomberman/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace Bomberman
 {
@@ -10,10 +12,54 @@ namespace Bomberman
         /// </summary>
         static void Main(string[] args)
         {
+            ProcessArgs(args);
+
             using (BmGame game = new BmGame())
             {
                 game.Run();
             }
+        }
+
+        [Conditional("DEBUG")]
+        private static void ProcessArgs(String[] args)
+        {
+            String batch = FindArg(args, "-B");
+            if (batch != null)
+            {
+                Process p = new Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = batch;
+                p.Start();
+
+                p.WaitForExit();
+                if (p.ExitCode == 0)
+                {
+                    String exec = FindArg(args, "-R");
+                    if (exec != null)
+                    {
+                        ProcessStartInfo info = new ProcessStartInfo();
+                        info.WorkingDirectory = Path.GetDirectoryName(exec);
+                        info.UseShellExecute = false;
+                        info.FileName = exec;
+
+                        p = Process.Start(info);
+                    }
+                }
+            }
+        }
+
+        private static String FindArg(String[] args, String prefix)
+        {
+            foreach (String arg in args)
+            {
+                if (arg.StartsWith(prefix))
+                {
+                    return arg.Substring(prefix.Length);
+                }
+            }
+
+            return null;
         }
     }
 #endif
