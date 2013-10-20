167717362104c25c4108caf6902d1f6286b9f228
Alex Lementuev <a.lementuev@gmail.com>
2013-10-19 22:20:22 -0700

    Added bit array

---
+++ BomberEngine/Util/BitArray.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BomberEngine.Util
+{
+    public class BitArray
+    {
+        public const int MaxLength = 8 * sizeof(long);
+
+        private long m_value;
+        private int m_length;
+
+        public BitArray(int length)
+        {
+            if (length < 0 || length > MaxLength)
+            {
+                throw new ArgumentException(String.Format("Length {0} is out of range 0..{1}", length, MaxLength));
+            }
+
+            m_length = length;
+        }
+
+        public bool this[int index]
+        {
+            get
+            {
+                CheckIndex(index);
+                return (m_value & (1L << index)) != 0;
+            }
+
+            set
+            {
+                CheckIndex(index);
+                if (value)
+                {
+                    m_value |= 1L << index;
+                }
+                else
+                {
+                    m_value &= ~(1L << index);
+                }
+            }
+        }
+
+        private void CheckIndex(int index)
+        {
+            if (index < 0 || index >= m_length)
+            {
+                throw new IndexOutOfRangeException(String.Format("Index {0} out of bounds 0..{1}", index, m_length));
+            }
+        }
+
+        public void Clear()
+        {
+            m_value = 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            BitArray other = obj as BitArray;
+            return other != null && other.m_length == m_length && other.m_value == m_value;
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)m_value;
+        }
+
+        public long value
+        {
+            get { return m_value; }
+        }
+
+        public int length
+        {
+            get { return m_length; }
+        }
+    }
+}
---
+++ BomberEngineTests/BitArrayTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BomberEngine.Util;
+
+namespace BomberEngineTests
+{
+    [TestClass]
+    public class BitArrayTests
+    {
+        [TestMethod]
+        public void TestEmpty()
+        {
+            BitArray array = new BitArray(10);
+            Assert.AreEqual(10, array.length);
+
+            for (int i = 0; i < array.length; ++i)
+            {
+                Assert.AreEqual(false, array[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestData()
+        {
+            BitArray array = new BitArray(10);
+
+            bool[] flags = new bool[10];
+            for (int i = 0; i < flags.Length; ++i)
+            {
+                array[i] = flags[i] = i % 2 != 0;
+            }
+
+            for (int i = 0; i < flags.Length; ++i)
+            {
+                Assert.AreEqual(flags[i], array[i]);
+            }
+        }
+
+        [TestMethod]
+        public void TestInvert()
+        {
+            BitArray array = new BitArray(1);
+
+            int i = 0;
+            array[i] = !array[i];
+
+            Assert.IsTrue(array[i]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestInvalidArgument()
+        {
+            new BitArray(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestInvalidArgument2()
+        {
+            new BitArray(BitArray.MaxLength + 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestInvalidIndex()
+        {
+            BitArray array = new BitArray(10);
+            array[-1] = true;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestInvalidIndex2()
+        {
+            BitArray array = new BitArray(10);
+            array[10] = true;
+        }
+    }
+}
+
+
