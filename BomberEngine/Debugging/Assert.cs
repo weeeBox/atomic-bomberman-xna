6c6040a331105aa2d782fbfd89e909573683bad3
Alex Lementuev <a.lementuev@gmail.com>
2013-11-09 13:18:01 -0800

    Added assert class

---
+++ BomberEngine/Debugging/Assert.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace BomberEngine
+{
+    public static class Assert
+    {
+        [Conditional("DEBUG")]
+        public static void True(bool condition, String format, params Object[] args)
+        {
+            System.Diagnostics.Debug.Assert(condition, "Assertion failed", format, args);
+        }
+
+        [Conditional("DEBUG")]
+        public static void True(bool condition, String message)
+        {
+            System.Diagnostics.Debug.Assert(condition, "Assertion failed", message);
+        }
+
+        [Conditional("DEBUG")]
+        public static void True(bool condition)
+        {
+            System.Diagnostics.Debug.Assert(condition, "Assertion failed");
+        }
+
+        [Conditional("DEBUG")]
+        public static void False(bool condition, String format, params Object[] args)
+        {
+            System.Diagnostics.Debug.Assert(!condition, "Assertion failed", format, args);
+        }
+
+        [Conditional("DEBUG")]
+        public static void False(bool condition, String message)
+        {
+            System.Diagnostics.Debug.Assert(!condition, "Assertion failed", message);
+        }
+
+        [Conditional("DEBUG")]
+        public static void False(bool condition)
+        {
+            System.Diagnostics.Debug.Assert(!condition, "Assertion failed");
+        }
+
+        [Conditional("DEBUG")]
+        public static void NotNull(Object o)
+        {
+            System.Diagnostics.Debug.Assert(o != null, "Assertion failed: object is null");
+        }
+
+        [Conditional("DEBUG")]
+        public static void Null(Object o)
+        {
+            System.Diagnostics.Debug.Assert(o == null, "Assertion failed: object is not null");
+        }
+
+        [Conditional("DEBUG")]
+        public static void Contains<T>(ICollection<T> collection, T element)
+        {
+            System.Diagnostics.Debug.Assert(collection.Contains(element), "Assertion failed");
+        }
+
+        [Conditional("DEBUG")]
+        public static void NotContains<T>(ICollection<T> collection, T element)
+        {
+            System.Diagnostics.Debug.Assert(!collection.Contains(element), "Assertion failed");
+        }
+
+        [Conditional("DEBUG")]
+        public static void Range(int index, Array array)
+        {
+            System.Diagnostics.Debug.Assert(index >= 0 && index < array.Length, "Assertion failed", "Index {0} is out of range {1}..{2}", index, 0, array.Length);
+        }
+
+        [Conditional("DEBUG")]
+        public static void Range(int index, int min, int max)
+        {
+            System.Diagnostics.Debug.Assert(index >= min && index < max, "Assertion failed", "Index {0} is out of range {1}..{2}", index, min, max);
+        }
+
+        [Conditional("DEBUG")]
+        public static void AreEqual(Object o1, Object o2)
+        {
+            System.Diagnostics.Debug.Assert(o1 == o2, "Assertion failed", "{0} != {1}", o1, o2);
+        }
+
+        [Conditional("DEBUG")]
+        public static void Fail(String format, params Object[] args)
+        {
+            System.Diagnostics.Debug.Assert(false, "Failed!", format, args);
+        }
+    }
+}
--- BomberEngine/Debugging/Debug.cs
+++ BomberEngine/Debugging/Debug.cs
@@ -44,59 +44,6 @@ namespace BomberEngine
         }
 
         [Conditional("DEBUG")]
-        public static void Assert(bool condition, String format, params Object[] args)
-        {
-            System.Diagnostics.Debug.Assert(condition, "Assertion failed", format, args);
-        }
-
-        [Conditional("DEBUG")]
-        public static void Assert(bool condition, String message)
-        {   
-            System.Diagnostics.Debug.Assert(condition, "Assertion failed", message);
-        }
-
-        [Conditional("DEBUG")]
-        public static void AssertNotNull(Object o)
-        {
-            System.Diagnostics.Debug.Assert(o != null, "Assertion failed: object is null");
-        }
-
-        [Conditional("DEBUG")]
-        public static void AssertNull(Object o)
-        {
-            System.Diagnostics.Debug.Assert(o == null, "Assertion failed: object is not null");
-        }
-
-        [Conditional("DEBUG")]
-        public static void Assert(bool condition)
-        {
-            System.Diagnostics.Debug.Assert(condition, "Assertion failed");
-        }
-
-        [Conditional("DEBUG")]
-        public static void AssertContains<T>(ICollection<T> collection, T element)
-        {
-            System.Diagnostics.Debug.Assert(collection.Contains(element), "Assertion failed");
-        }
-            
-        [Conditional("DEBUG")]
-        public static void AssertNotContains<T>(ICollection<T> collection, T element)
-        {
-            System.Diagnostics.Debug.Assert(!collection.Contains(element), "Assertion failed");
-        }
-
-        [Conditional("DEBUG")]
-        public static void AssertRange(int index, Array array)
-        {
-            System.Diagnostics.Debug.Assert(index >= 0 && index < array.Length, "Assertion failed", "Index {0} is out of range {1}..{2}", index, 0, array.Length);
-        }
-
-        [Conditional("DEBUG")]
-        public static void AssertRange(int index, int min, int max)
-        {
-            System.Diagnostics.Debug.Assert(index >= min && index < max, "Assertion failed", "Index {0} is out of range {1}..{2}", index, min, max);
-        }
-
         [Conditional("DEBUG")]
         public static void Fail(String format, params Object[] args)
         {
